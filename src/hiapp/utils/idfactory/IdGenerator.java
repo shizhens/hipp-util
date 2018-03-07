ad521eaa92dbd0cd11c1427c9fb17e2e01360e22
zhang <zhang@DESKTOP-O67D9B2>
2018-03-07 15:14:17 +0800

    讲commons-lang3换成commons-lang 2.5

--- src/hiapp/utils/idfactory/IdGenerator.java
+++ src/hiapp/utils/idfactory/IdGenerator.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.lang3.time.DateUtils;
+import org.apache.commons.lang.time.DateUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
